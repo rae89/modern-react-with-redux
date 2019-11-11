fc87b98b5cec6f3ad8a3dfc647bcadedffe49607
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-10 16:40:01 -0800

    correct jsx keyword

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,7 +10,7 @@ const App = function() {
   const labelText = 'Enter name:';
   return (
     <div>
-        <label className="label" for="name">{labelText}:</label>
+        <label className="label" htmlFor="name">{labelText}:</label>
         <input id="name" type="text" />
         <button style={{ backgroundColor: 'blue', color: 'white' }}>
             {buttonText.text}
