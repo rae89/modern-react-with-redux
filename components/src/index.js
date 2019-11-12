dc612151f3dcd6b9fcbc8a4fec4a8e1d22cd9990
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-11 20:17:47 -0800

    more passing props

--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-const App = () =>{
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM"/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:00PM"/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM"/>
-        </div>
-    );
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice blog post"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        content="I like the subject"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="I like the writing"
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root')); 
+ReactDOM.render(<App />, document.querySelector("#root"));
