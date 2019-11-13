e6b560f24579540067a9575d64143e0324b36d74
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-12 22:14:49 -0800

    ternary expressions in jsx

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const getSeason = (lat, month) => {
-    if (month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
-    } else {
-        return lat > 0 ? 'winter' : 'summer';
-    }
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
 };
 
-const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth());
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
 
-    console.log(season);
-
-    return <div>Season Display</div>;
+  return <div><h1>{text}</h1></div>;
 };
 
 export default SeasonDisplay;
