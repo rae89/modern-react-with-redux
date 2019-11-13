8cfb0dc565eb304f82e085213916a2be32d39d9e
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-12 21:38:30 -0800

    conditionally rendering content

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,21 +7,31 @@ class App extends React.Component {
     super(props);
 
     // THIS IS THE ONLY TIME we do direct assignment
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       // we called setstate!
       position => {this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState( {errorMessage:err.message });
+      }
     );
   }
 
   // React says we have to define render!
   render() {
-    return <div>Latitude: {this.state.lat}</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>;
   }
-}
+} 
 
 
 ReactDOM.render(<App />, document.querySelector("#root"));
