61fa860d363696e5e80a7b82b13e22a066a39c29
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-12 21:54:22 -0800

    refactoring data loading to lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,37 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     // THIS IS THE ONLY TIME we do direct assignment
-    this.state = { lat: null, errorMessage: '' };
+    this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      // we called setstate!
-      position => {this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState( {errorMessage:err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   // React says we have to define render!
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <div>Latitude: {this.state.lat}</div>;
     }
 
     return <div>Loading!</div>;
   }
-} 
-
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
