b066aae6d658dd8952e49b38595dc6a1b6c18ef0
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-24 11:23:19 -0800

    add get request to unsplash

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
+
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID e725f0e22e7dd9ca6129af47a675b67a8e888ad425aa020fecf4553fa25f1ae6'
+      }
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
