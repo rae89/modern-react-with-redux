f70132d24249b50b7c1bcce58c144f35638549dd
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-24 12:22:43 -0800

    add image list ocomponent

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
-
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
 
-  onSearchSubmit = async (term) => {
+  onSearchSubmit = async term => {
     const response = await unsplash.get('/search/photos', {
       params: {query: term},
       
@@ -18,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
