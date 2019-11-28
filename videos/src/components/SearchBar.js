53e5411cdce833022c1ddbb68155cba2b60f0e41
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-28 11:27:20 -0800

    putting it all together

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -10,8 +10,7 @@ class SearchBar extends React.Component {
     onFormSubmit = event => {
         event.preventDefault();
 
-        // TODO: Make sure we call
-        // callbcak from parent component
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
