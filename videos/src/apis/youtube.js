415d4c0ffaa13d1f407f953116b3bd7f20a2694a
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-29 08:57:22 -0800

    rendering a list of vidoes

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const KEY = 'AIzaSyDLbuo0WsAxsJHccC9ieoMpQHXLxxxtldM';
+const KEY = "AIzaSyDLbuo0WsAxsJHccC9ieoMpQHXLxxxtldM";
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
-    }
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
 });
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
-    state = { videos: [] };
-    onTermSubmit = async term => {
-        const response = await youtube.get('./search', {
-            params: {
-                q: term
-            }
-        });
+  state = { videos: [] };
+  onTermSubmit = async term => {
+    const response = await youtube.get("./search", {
+      params: {
+        q: term
+      }
+    });
 
-        this.setState({ videos: response.data.items });
-    };
+    this.setState({ videos: response.data.items });
+  };
 
-    render() {
-        return (
-            <div>
-                <SearchBar onFormSubmit={this.onTermSubmit}/>
-                I have {this.state.videos.length} videos.
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 export default App;
