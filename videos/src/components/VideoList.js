a3613a8dc5a37c9b55f6bc8e92cf55465a52d3ce
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-11-29 20:34:23 -0800

    fixing a few warnings

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,10 +2,16 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
-  const renderedList = videos.map((video) => {
-    return <VideoItem  onVideoSelect={onVideoSelect} video={video} />;
+  const renderedList = videos.map(video => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
-export default VideoList; 
+export default VideoList;
