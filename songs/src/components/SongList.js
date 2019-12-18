7a3250d17bbb9ebda0cc93585b89a4b6afc6553b
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2019-12-17 20:58:33 -0800

    configuring connect with MapStateToProps

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -7,4 +7,10 @@ class SongList extends Component {
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  console.log(state);
+
+  return state;
+};
+
+export default connect(mapStateToProps)(SongList);
