574cbe9ba4d8a96260dc8df8dbf5b6c1ef75688c
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-01-26 21:10:14 -0800

    finding unique user ids

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions";
+import { fetchPosts, fetchPostsAndUsers } from "../actions";
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -34,5 +34,5 @@ const mapStateToProps = (state) => {
   return { posts: state.posts };
 };
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
  
