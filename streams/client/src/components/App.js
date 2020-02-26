8bff9a0850f9f9189dfc6cfebc1c7883937131c7
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-02-25 22:49:41 -0800

    navigating with react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+   );
 };
 
 const PageTwo = () => {
-  return <div>
+  return (
+  <div>
     PageTwo
     <button>Click Me!</button>
-  </div>;
+    <Link to="/">Navigate to Page One</Link>
+  </div>
+  );
 };
 
 const App = () => {
